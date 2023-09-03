cd85d91e2b23be099640b17ae9bed7a38a3273bb
woutkolkman <wout.kolkman1@hotmail.com>
2023-09-03 12:10:59 +0200

    init

---
+++ ThinkRing/Hooks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using MonoMod.RuntimeDetour;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace ThinkRing
+{
+    class Hooks
+    {
+        public static void Apply()
+        {
+            //initialize options & load sprites
+            On.RainWorld.OnModsInit += RainWorldOnModsInitHook;
+        }
+
+
+        //initialize options & load sprites
+        static void RainWorldOnModsInitHook(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig(self);
+            MachineConnector.SetRegisteredOI(Plugin.GUID, new Options());
+        }
+
+
+        //at framerate
+        static void RainWorldGameRawUpdateHook(On.RainWorldGame.orig_RawUpdate orig, RainWorldGame self, float dt)
+        {
+            orig(self, dt);
+        }
+    }
+}
---
+++ ThinkRing/Options.cs
@@ -0,0 +1,142 @@
+﻿using Menu.Remix.MixedUI;
+using UnityEngine;
+using System;
+
+namespace ThinkRing
+{
+    public class Options : OptionInterface
+    {
+        public int curTab;
+
+
+        public Options()
+        {
+
+        }
+
+
+        public override void Initialize()
+        {
+            const float startHeight = 520f;
+
+            base.Initialize();
+            Tabs = new OpTab[]
+            {
+                new OpTab(this, "General")
+            };
+
+            /**************** General ****************/
+            curTab = 0;
+            AddTitle();
+            float x = 90;
+            float y = startHeight;
+        }
+
+
+        private void AddTitle()
+        {
+            OpLabel title = new OpLabel(new Vector2(150f, 560f), new Vector2(300f, 30f), Plugin.Name, bigText: true);
+            OpLabel version = new OpLabel(new Vector2(150f, 540f), new Vector2(300f, 30f), $"Version {Plugin.Version}");
+
+            Tabs[curTab].AddItems(new UIelement[]
+            {
+                title,
+                version
+            });
+        }
+
+
+        private void AddIcon(Vector2 pos, string iconName)
+        {
+            Tabs[curTab].AddItems(new UIelement[]
+            {
+                new OpImage(pos, iconName)
+            });
+        }
+
+
+        private void AddCheckbox(Configurable<bool> option, Vector2 pos, Color? c = null)
+        {
+            if (c == null)
+                c = Menu.MenuColorEffect.rgbMediumGrey;
+
+            OpCheckBox checkbox = new OpCheckBox(option, pos)
+            {
+                description = option.info.description,
+                colorEdge = (Color)c
+            };
+
+            OpLabel label = new OpLabel(pos.x + 40f, pos.y + 2f, option.info.Tags[0] as string)
+            {
+                description = option.info.description,
+                color = (Color)c
+            };
+
+            Tabs[curTab].AddItems(new UIelement[]
+            {
+                checkbox,
+                label
+            });
+        }
+
+
+        private void AddKeyBinder(Configurable<KeyCode> option, Vector2 pos, Color? c = null)
+        {
+            if (c == null)
+                c = Menu.MenuColorEffect.rgbMediumGrey;
+
+            OpKeyBinder keyBinder = new OpKeyBinder(option, pos, new Vector2(100f, 30f), false)
+            {
+                description = option.info.description,
+                colorEdge = (Color)c
+            };
+
+            OpLabel label = new OpLabel(pos.x + 100f + 16f, pos.y + 5f, option.info.Tags[0] as string)
+            {
+                description = option.info.description,
+                color = (Color)c
+            };
+
+            Tabs[curTab].AddItems(new UIelement[]
+            {
+                keyBinder,
+                label
+            });
+        }
+
+
+        private void AddComboBox(Configurable<string> option, Vector2 pos, string[] array, float width = 80f, FLabelAlignment alH = FLabelAlignment.Center, OpLabel.LabelVAlignment alV = OpLabel.LabelVAlignment.Center)
+        {
+            OpComboBox box = new OpComboBox(option, pos, width, array)
+            {
+                description = option.info.description
+            };
+
+            Vector2 offset = new Vector2();
+            if (alV == OpLabel.LabelVAlignment.Top) {
+                offset.y += box.size.y + 5f;
+            } else if (alV == OpLabel.LabelVAlignment.Bottom) {
+                offset.y += -box.size.y - 5f;
+            } else if (alH == FLabelAlignment.Right) {
+                offset.x += box.size.x + 20f;
+                alH = FLabelAlignment.Left;
+            } else if (alH == FLabelAlignment.Left) {
+                offset.x += -box.size.x - 20f;
+                alH = FLabelAlignment.Right;
+            }
+
+            OpLabel label = new OpLabel(pos + offset, box.size, option.info.Tags[0] as string)
+            {
+                description = option.info.description
+            };
+            label.alignment = alH;
+            label.verticalAlignment = OpLabel.LabelVAlignment.Center;
+
+            Tabs[curTab].AddItems(new UIelement[]
+            {
+                box,
+                label
+            });
+        }
+    }
+}
